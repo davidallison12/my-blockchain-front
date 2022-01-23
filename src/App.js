7b5d3287db297c06baf8b0dced95caac99ba5007
david-allison12 <dgreenal@gmail.com>
2022-01-22 23:39:39 -0500

    Changed up style of react front end

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ export default function App() {
 
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+       👋🏾 Hey there! Welcome to PokePortal <img class="emojidex-emoji" src="https://cdn.emojidex.com/emoji/seal/Pokeball.png" emoji-code="Pokeball" alt="Pokeball" />
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am david and I am a blockchain enthusiast. Connect your Ethereum wallet and let me know your ride or die Pokemon team!
         </div>
 
         <button className="waveButton" onClick={wave}>
